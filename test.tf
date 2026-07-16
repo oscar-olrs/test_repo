03d5bde8f1f66184e50c660b027796e8409b3b96
oscar-olrs <oscar@olrstech.com>
2026-07-16 14:31:49 -0700

    Pushing from Git to GitHub

--- test.tf
+++ test.tf
@@ -4,3 +4,5 @@ us-west-2
 
 
 #I just edited this on GitHub Cloud and going to do a git pull and see what happens, I want to see if it updates my local copies.
+
+#Now I will edit file on local then do git Pull
